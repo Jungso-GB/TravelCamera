fe7e8538a96b703ca0d7b5976c38f253d26dd174
ARK-BLKCN <48032984+sernoz@users.noreply.github.com>
2023-11-05 17:39:53 +0100

    Add Brio scripts for GPose control [THANKS]

---
+++ TravelCamera/Core/IService.cs
@@ -0,0 +1,13 @@
+﻿//Made by Brio repo
+using System;
+
+namespace TravelCamera.Core;
+
+public interface IService : IDisposable
+{
+    void AssignInstance();
+    void ClearInstance();
+    void Start();
+    void Tick(float delta);
+    void Stop();
+}
---
+++ TravelCamera/Core/ServiceBase.cs
@@ -0,0 +1,35 @@
+﻿//Made by Brio repo
+using System;
+
+namespace TravelCamera.Core;
+
+public abstract class ServiceBase<T> : IService
+    where T : ServiceBase<T>
+{
+    private static T? _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if(_instance == null)
+                throw new Exception($"No service found: {typeof(T)}");
+
+            return _instance;
+        }
+    }
+
+    public void AssignInstance() => _instance = (T?)this;
+    public void ClearInstance() => _instance = null;
+
+    public virtual void Start()
+    {
+        _instance = (T)this;
+    }
+
+    public virtual void Tick(float delta) { }
+
+    public virtual void Stop() { }
+
+    public virtual void Dispose() { }
+}
